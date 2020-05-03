fa66ad2d47045a2396c51c759a6292cf7c47c14d
Nathan Foote <nfootega@gmail.com>
2020-05-03 16:58:59 -0400

    Title menu

---
+++ Apex Dungeon/Assets/Scripts/scrollHorizontal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollHorizontal : MonoBehaviour
+{
+    public float speed = 1;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, 0);
+        if (transform.position.x > 1280*1.5f)
+        {
+            transform.position = new Vector3(0 - (1280/2), transform.position.y, 0);
+        }
+    }
+}
---
+++ Apex Dungeon/Assets/Scripts/titleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class titleController : MonoBehaviour
+{
+    public Button play;
+    public Button options;
+
+    void Start()
+    {
+        play.onClick.AddListener(playListener);
+        options.onClick.AddListener(optionsListener);
+    }
+
+    void playListener()
+    {
+        SceneManager.LoadScene("test", LoadSceneMode.Single);
+    }
+
+    void optionsListener()
+    {
+
+    }
+
+}
