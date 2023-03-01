034a7876d0780060e5a21031732bb28e85e31ff0
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2023-03-01 11:31:52 -0600

    Organize scripts

---
+++ Apex Dungeon/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,344 @@
+﻿using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gmInstance = null;
+    public DungeonObject Dungeon;
+    public DungeonGenerator DunGen;
+    public SkillGenerator SkillGenerator;
+    public LogManager Log;
+    public bool playersTurn = false;
+    
+    private List<Enemy> enemies;
+    private List<Furniture> furniture;
+    private List<Chest> chests;
+    private List<Trap> traps;
+
+    private bool enemiesTurn;
+    public bool doingSetup = true;
+
+    public GameObject tileCursor;
+    GameObject cursor;
+    public float mRow;
+    public float mCol;
+
+    public string DungeonName = "Misty Dungeon";
+    public int level;
+    public int score;
+    public bool gameStarted = false;
+    public List<(string, int)> scores;
+    public string playerName;
+    public string state = "play";
+
+    private int cursorRow, cursorCol;
+
+    private void Awake()
+    {
+        if(gmInstance == null)
+        {
+            gmInstance = this;
+        }else if(gmInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        level = Data.floor;
+        score = 0;
+        scores = new List<(string, int)>();
+        playerName = Data.playerName;
+        
+        enemies = new List<Enemy>();
+        furniture = new List<Furniture>();
+        chests = new List<Chest>();
+        traps = new List<Trap>();
+        Setup();
+
+        Vector3 position = new Vector3(0f, 0f, 0f);
+        cursor = Instantiate(tileCursor, position, Quaternion.identity) as GameObject;
+
+        Log = GameObject.FindGameObjectWithTag("Log").GetComponent<LogManager>();
+    }
+
+    void Setup()
+    {
+        if(SceneManager.GetActiveScene().name == "Dungeon") {
+            Dungeon = null;
+            Dungeon = DunGen.Initalize();
+            
+        }
+    }
+
+    public void Reset(){
+        Dungeon = DunGen.Reset();
+    }
+
+    public void FullReset(){
+        Data.reset();
+        level = 1;
+        score = 0;
+        enemies.Clear();
+        furniture.Clear();
+        chests.Clear();
+        traps.Clear();
+    }
+
+    void Update()
+    {
+        
+
+        if (state == "score"){
+
+            return;
+        }
+        if(state == "menu"){
+            
+            return;
+        }
+        if(state == "play" && cursor == null){
+            level = 1;
+            score = 0;
+            enemies.Clear();
+            furniture.Clear();
+            chests.Clear();
+            traps.Clear();
+            Vector3 position = new Vector3(0f, 0f, 0f);
+            cursor = Instantiate(tileCursor, position, Quaternion.identity) as GameObject;
+            Dungeon = DunGen.Initalize();
+        }
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+
+        mRow = Mathf.Round(pz.y);
+        mCol = Mathf.Round(pz.x);
+
+        Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        //updating grid cursor image
+        if (Input.GetButtonDown("Fire1"))
+        {
+            cursor.SetActive(true);
+            cursor.transform.position = new Vector3(mCol, mRow, 0f);
+            cursorRow = (int)mRow;
+            cursorCol = (int)mCol;
+            if (player.isBlocked((int)mRow, (int)mCol))
+            {
+                cursor.GetComponent<Cursor>().SetRed();
+            }
+            else
+            {
+                cursor.GetComponent<Cursor>().SetGreen();
+            }
+        }
+        
+        if (player.getRow() == cursorRow && player.getCol() == cursorCol)
+        {
+            cursor.SetActive(false);
+        }
+
+
+        if (playersTurn || enemiesTurn || doingSetup)
+        {
+            return;
+        }
+
+        if (!enemiesTurn)
+        {
+            enemiesTurn = true;
+            MoveEnemies();
+        }
+    }
+
+    public void removeEnemy(Enemy e)
+    {
+        enemies.Remove(e);
+    }
+
+    public void clearEnemies()
+    {
+        enemies.Clear();
+    }
+
+    public void clearFurniture()
+    {
+        furniture.Clear();
+    }
+
+    public void clearChests()
+    {
+        chests.Clear();
+    }
+
+    public void clearTraps()
+    {
+        traps.Clear();
+    }
+
+    public void removeFurniture(Furniture e)
+    {
+        furniture.Remove(e);
+    }
+
+    public void removeChest(Chest e)
+    {
+        chests.Remove(e);
+    }
+
+    public void removeTrap(Trap t)
+    {
+        traps.Remove(t);
+    }
+
+    public Enemy getEnemyAtLoc(int r, int c)
+    {
+        if (enemies.Count == 0)
+        {
+            return null;
+        }
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            int er = enemies[i].getRow();
+            int ec = enemies[i].getCol();
+            if(r == er && c == ec)
+            {
+                return enemies[i];
+            }
+            
+        }
+        return null;
+    }
+
+    public Trap GetTrapAtLoc(int r, int c)
+    {
+        if (traps.Count == 0)
+        {
+            return null;
+        }
+        for (int i = 0; i < traps.Count; i++)
+        {
+            int tr = traps[i].GetRow();
+            int tc = traps[i].GetCol();
+            if (r == tr && c == tc)
+            {
+                return traps[i];
+            }
+
+        }
+        return null;
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    public void AddTrapToList(Trap trap)
+    {
+        traps.Add(trap);
+    }
+
+    public void AddFurnitureToList(Furniture f)
+    {
+        furniture.Add(f);
+    }
+
+    public void AddChestToList(Chest c)
+    {
+        chests.Add(c);
+    }
+
+    public Furniture getFurnitureAtLoc(int r, int c)
+    {
+        if (furniture.Count == 0)
+        {
+            return null;
+        }
+        for (int i = 0; i < furniture.Count; i++)
+        {
+            int er = furniture[i].getRow();
+            int ec = furniture[i].getCol();
+            if (r == er && c == ec)
+            {
+                return furniture[i];
+            }
+
+        }
+        return null;
+    }
+
+    public Chest getChestAtLoc(int r, int c)
+    {
+        if (chests.Count == 0)
+        {
+            return null;
+        }
+        for (int i = 0; i < chests.Count; i++)
+        {
+            int er = chests[i].getRow();
+            int ec = chests[i].getCol();
+            if (r == er && c == ec)
+            {
+                return chests[i];
+            }
+
+        }
+        return null;
+    }
+
+    void MoveEnemies()
+    {
+        if(enemies.Count == 0)
+        {
+            playersTurn = true;
+            enemiesTurn = false;
+            return;
+        }
+        for(int i = 0;i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            //WAIT OPTIONAL
+        }
+        playersTurn = true;
+        enemiesTurn = false;        
+    }
+
+    public void UpdateCursor(string state, int row, int col)
+    {
+        //if (state == "Furniture")
+        //{
+        //    //look for nearest open square
+        //    cursor.SetActive(true);
+        //    cursor.transform.position = new Vector3(col, row, 0f);
+        //    cursor.GetComponent<Cursor>().SetRed();
+        //}
+        //if (state == "Attack")
+        //{
+        //    cursor.SetActive(true);
+        //    cursor.transform.position = new Vector3(col, row, 0f);
+        //    cursor.GetComponent<Cursor>().SetRed();
+        //    //cursorRow = (int)mRow;
+        //    //cursorCol = (int)mCol;
+        //}
+        //if (state == "Player")
+        //{
+        //    cursor.SetActive(true);
+        //    cursor.transform.position = new Vector3(col, row, 0f);
+        //    cursor.GetComponent<Cursor>().SetGreen();
+        //}
+        //if (state == "Blocked")
+        //{
+        //    cursor.SetActive(true);
+        //    cursor.transform.position = new Vector3(col, row, 0f);
+        //    cursor.GetComponent<Cursor>().SetRed();
+        //}
+    }
+
+    public void UpdateCursor(string state)
+    {
+        //if (state == "Interrupt")
+        //{
+        //    cursor.GetComponent<Cursor>().SetRed();
+        //}
+    }
+}
---
+++ Apex Dungeon/Assets/Scripts/MovingEntities/MovingEntity.cs
@@ -0,0 +1,532 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using static StatusEffect;
+
+public abstract class MovingEntity : MonoBehaviour
+{
+    public string entityName;
+
+    protected int maxHp;
+    protected int maxMp;
+    protected int hp;
+    protected int baseHp;
+    protected int mp;
+    protected int expLevel;
+    protected int exp;
+    protected int maxExp;
+
+    // Entity Stats
+    protected int attack;
+    protected int defense;
+    protected int strength;
+    protected int intelligence;
+    protected int critical;
+    protected int evade;
+    protected int blockStat;
+
+    // Combat
+    protected int attackRange;
+
+    // Resistances
+    protected float fireResistance;
+    protected float iceResistance;
+    protected float lightningResistance;
+    protected float poisonResistance;
+
+    // Active Effects
+    public bool silenced;
+    public bool stealth;
+    public bool invisible;
+    public bool root;
+
+    protected float attackScale = 1f;
+    protected float defenseScale = 1f;
+    protected float strengthScale = 1f;
+    protected float intelligenceScale = 1f;
+    protected float criticalScale = 1f;
+    protected float evadeScale = 1f;
+    protected float blockScale = 1f;
+
+    protected bool dead;
+
+    public bool agro;
+    public int agroRange;
+
+    public List<StatusEffect> statusEffects;
+    public List<Skill> skills;
+
+    public Queue<(string, Color)> popupTexts;
+    public bool canDisplayPopupText = true;
+    [SerializeField] public float popupTextDelay = .4f;
+
+    public float moveTime = 0.1f;
+    public float speed = 3f;
+    public LayerMask blockingLayer;
+
+    public ParticleSystem blood;
+    public GameObject damageText;
+
+    protected BoxCollider2D boxCollider;
+
+    private Vector2 target;
+    protected bool atTarget = true;
+
+    protected int row;
+    protected int col;
+
+    protected int type = 2;
+
+    private Pathfinder pathing = new Pathfinder();
+    private Path path = new Path();
+
+    protected bool moving = false;
+    protected bool moved = true;
+    
+    protected virtual void Start()
+    {
+        dead = false;
+        boxCollider = GetComponent<BoxCollider2D>();
+        row = (int)transform.position.y;
+        col = (int)transform.position.x;
+
+        statusEffects = new List<StatusEffect>();
+        popupTexts = new Queue<(string, Color)> ();
+        canDisplayPopupText = true;
+        skills = new List<Skill>();
+    }
+
+    bool checkValidPath()
+    {
+        if (path == null || (path.nodes.Count == 0))
+        {
+            doneMoving();
+            return false;
+        }
+        return true;
+    }
+
+    public void CancelPath()
+    {
+        moving = false;
+        atTarget = true;
+    }
+
+    bool checkValidTile(Vector2 next)
+    {
+        if (GameManager.gmInstance.Dungeon.tileMap[(int)next.y, (int)next.x].getBlocked())
+        {
+            doneMoving();
+            return false;
+
+        }
+        return true;
+    }
+
+    void setMapOccupancy()
+    {
+        GameManager.gmInstance.Dungeon.tileMap[row, col].occupied = 0;
+        GameManager.gmInstance.Dungeon.tileMap[(int)target.y, (int)target.x].occupied = type;
+    }
+
+    public GameObject SpawnObject(GameObject o, Vector2 pos)
+    {
+        return Instantiate(o, pos, Quaternion.identity);
+    }
+
+    void updateLocalPosition()
+    {
+        row = (int)target.y;
+        col = (int)target.x;
+    }
+
+    protected bool Move(int r, int c)
+    {
+        Tile startTile = GameManager.gmInstance.Dungeon.tileMap[row, col];
+        Tile endTile = GameManager.gmInstance.Dungeon.tileMap[r, c];
+        
+        //If destination is current location
+        if (row == r && col == c){
+            return false;
+        }
+
+        if (!moving && atTarget)
+        {
+            if (GameManager.gmInstance.Dungeon.tileMap[r, c].getWall())
+            {
+                return false;
+            }
+            path = pathing.findPath(startTile, endTile);
+
+            if (!checkValidPath()){
+                return false;
+            }
+            
+            Vector2 next = path.nodes.Peek();
+
+            if (!checkValidTile(next))
+            {
+                return false;
+            }
+
+            moving = true;
+            atTarget = false;
+            target = path.nodes.Dequeue();
+
+            setMapOccupancy();
+            updateLocalPosition();
+
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void setNextTarget()
+    {
+        if (atTarget)
+        {
+            if (!checkValidPath())
+            {
+                return;
+            }
+            else
+            {
+                target = path.nodes.Dequeue();
+
+                if (!GameManager.gmInstance.Dungeon.tileMap[(int)target.y, (int)target.x].getBlocked())
+                {
+                    moving = true;
+                    atTarget = false;
+
+                    setMapOccupancy();
+                    updateLocalPosition();
+                }
+                else
+                {
+                    //Interupt Path
+                    doneMoving();
+                    return;
+                }
+            }
+        }   
+    }
+
+    protected virtual void Update()
+    {
+        if (GameManager.gmInstance.Dungeon == null) return;
+
+        if (!atTarget)
+        {
+            moveToTarget();
+        }
+
+        if (canDisplayPopupText && popupTexts.Count > 0)
+        {
+            canDisplayPopupText = false;
+            var values = popupTexts.Dequeue();
+            StartCoroutine(SpawnText(values.Item1, values.Item2));
+        }
+    }
+
+    protected void moveToTarget()
+    {
+        float distancex = target.x - transform.position.x;
+        float distancey = target.y - transform.position.y;
+
+        if (distancex > 0)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        if (distancex < 0)
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+        if (distancey > 0)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+        }
+        if (distancey < 0)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+        }
+
+        if (Mathf.Abs(distancex) < 0.1 && Mathf.Abs(distancey) < 0.1)
+        {
+            atTarget = true;
+            transform.position = new Vector2(target.x, target.y);
+            if ((path.nodes.Count == 0))
+            {
+                moving = false;
+            }
+        }
+    }
+
+    protected virtual bool AttemptMove <T>(int r, int c) where T : Component
+    {
+        bool canMove = Move(r, c);
+
+        if(!canMove)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    protected abstract void OnCantMove<T>(T Component) where T : Component;
+
+    public float calculateDamage(float m = 1f)
+    {
+        int scaledStrength = (int)(strength * strengthScale);
+        float increase = (float)(attack * (scaledStrength * 0.05));
+        float attackDamage = (float)(attack + increase);
+        attackDamage *= m;
+        return -attackDamage;
+    }
+
+    public virtual void takeDamage(float change, Color color, bool critical = false, bool canDodge = true)
+    {
+        hp += (int)change;
+        change = Mathf.Floor(change);
+
+        if (change < 0)
+        {
+            if (critical)
+            {
+                AddTextPopup($"CRIT! {(int)change}", color);
+            }
+            else
+            {
+                AddTextPopup($"{(int)change}", color);
+            }
+            moving = false;
+           
+            SpawnBlood();
+        }
+        else
+        {
+            SoundManager.sm.PlayHealSound();
+            AddTextPopup($"+{(int)change}", color);
+        }
+        
+        if(hp <= 0)
+        {
+            dead = true;
+            SpawnBlood();
+        }
+        if (hp > maxHp)
+        {
+            hp = maxHp;
+        }
+    }
+
+    public virtual void AddTextPopup(string text, Color color)
+    {
+        popupTexts.Enqueue((text, color));
+    }
+
+    private IEnumerator SpawnText(string text, Color color)
+    {
+        canDisplayPopupText = false;
+        GameObject popup = GameObject.Instantiate(damageText, new Vector3(this.transform.position.x, this.transform.position.y, 0), Quaternion.identity);
+        popup.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = $"{text}";
+        popup.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().color = color;
+        yield return new WaitForSeconds(popupTextDelay);
+        canDisplayPopupText = true;
+    }
+
+    protected void SpawnBlood()
+    {
+        ParticleSystem bloodSpray = Instantiate(blood, transform.position, Quaternion.identity);
+        bloodSpray.transform.parent = transform;
+    }
+
+    public virtual void AddStatusEffect(StatusEffect se)
+    {
+        statusEffects.Add(se);
+        AddTextPopup(se.popupText, se.textColor);
+
+        GameManager.gmInstance.Log.AddLog($">{entityName} is afflicted with {se.popupText}.");
+
+        if (se.order == StatusEffect.EffectOrder.Status)
+        {
+            se.Activate(this);
+        }
+    }
+
+    protected void UpdateStatusEffectDuration()
+    {
+        foreach (StatusEffect e in statusEffects)
+        {
+            e.duration -= 1;
+            if (e.duration == 0) e.Deactivate(this);
+        }
+        statusEffects.RemoveAll(e => e.duration == 0);
+    }
+
+    public virtual void RemoveAllStatusEffect(EffectType type)
+    {
+        foreach (StatusEffect e in statusEffects)
+        {
+            if (e.effectId == type)
+            {
+                e.Deactivate(this);
+            }
+        }
+        statusEffects.RemoveAll(e => e.effectId == type);
+    }
+
+    protected virtual void ApplyStatusEffects(string time)
+    {
+        if (time == "start")
+        {
+            foreach (StatusEffect e in statusEffects)
+            {
+                if (e.order == EffectOrder.Start) e.Activate(this);
+            }
+        }
+        if (time == "end")
+        {
+            foreach (StatusEffect e in statusEffects)
+            {
+                if (e.order == EffectOrder.End) e.Activate(this);
+            }
+        }
+    }
+
+    public virtual void SkipTurn()
+    {
+        Debug.Log("ENTITY SKIP TURN");
+    }
+
+    //public Enumerable PauseForSeconds(float seconds)
+    //{
+
+    //}
+
+    public virtual void CalculateStats()
+    {
+
+    }
+
+    public void addMP(float change)
+    {
+        mp += (int)change;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+
+    public int getHP()
+    {
+        return hp;
+    }
+
+    public int getMP()
+    {
+        return mp;
+    }
+
+    public int getMaxHP()
+    {
+        return maxHp;
+    }
+
+    public int getMaxMP()
+    {
+        return maxMp;
+    }
+
+    private void doneMoving()
+    {
+        moving = false;
+        atTarget = true;
+    }
+
+    public float getStrengthScale()
+    {
+        return strengthScale;
+    }
+    public float getCriticalScale()
+    {
+        return criticalScale;
+    }
+    public float getDefenseScale()
+    {
+        return defenseScale;
+    }
+    public float getEvadeScale()
+    {
+        return evadeScale;
+    }
+
+    public void setStrengthScale(float f)
+    {
+        strengthScale = f;
+    }
+    public void setDefenseScale(float f)
+    {
+        defenseScale = f;
+    }
+    public void setCriticalScale(float f)
+    {
+        criticalScale = f;
+    }
+    public void setEvadeScale(float f)
+    {
+        evadeScale = f;
+    }
+
+    private void UpdateShadows(int r, int c)
+    {
+        GameManager.gmInstance.Dungeon.UpdateShadows(r, c);
+    }
+
+    public void setHP(int i){
+        this.hp = i;
+        if (hp > maxHp) hp = maxHp;
+        if (hp < 0) hp = 0;
+    }
+
+    public void addHp(int i)
+    {
+        this.hp += i;
+        if (hp > maxHp) hp = maxHp;
+        if (hp < 0) hp = 0;
+    }
+
+    public void addMp(int i)
+    {
+        this.mp += i;
+        if (i > 0)
+        {
+            AddTextPopup($"MP +{i}", ColorManager.MANA);
+        }
+        if (i < 0)
+        {
+            AddTextPopup($"MP {i}", ColorManager.MANA);
+        }
+
+        if (mp > maxMp) mp = maxMp;
+        if (mp < 0) mp = 0;
+    }
+
+    public void setPosition(int r, int c){
+        GameManager.gmInstance.Dungeon.tileMap[row,col].occupied = 0;
+        gameObject.transform.position = new Vector3(c, r, 0f);
+        this.row = r;
+        this.col = c;
+        GameManager.gmInstance.Dungeon.tileMap[r,c].occupied = 1;
+    }
+
+
+}
